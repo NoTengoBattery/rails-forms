75ef88571f19c0791d25fbe799e7f9b63bf63d92
ammarkandel <merom8703@gmail.com>
2021-02-12 19:52:18 +0200

    modify edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.new(user_params)
 
-    if @user.save
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
         redirect_to edit_user_path
     else
         render('edit')
